dd4b5737b2410f5c53486da280e2c116b4395ee2
gerald-lindner <gerald.lindner@stud.sbg.ac.at>
2016-07-10 11:37:29 +0200

    changed dirs, added xslx.
    next:
    -see if ordered
    -easier way to export list
    -directly input from scrapy
    feed:http://stackoverflow.com/questions/37882663/scrapy-how-to-populate-item-with-data-from-pandas-dataframe

--- Test1.py
+++ Test1.py
@@ -9,12 +9,12 @@ import re #for finding patterns
 import pandas as pd #for handling tables
 from scrapy.crawler import CrawlerProcess #neccasary
 url=[]#list of url
-number=[] #the actual counting number
+number=[1] #the actual counting number
 
-names_path="C:/Users/geri/Dropbox/2_BTSync Sync/04_Uni/02_MA Arbeit/ODA_2014_names_test.csv"
+names_path="D:/Dropbox/Dropbox/2_BTSync Sync/04_Uni/02_MA Arbeit/ODA_2014_names_test.csv"
 names=pd.read_csv(names_path, encoding='latin1',sep=';')
 
-for y in range(1,6):#creating the urls, must be done in loop before
+for y in range(0,6):#creating the urls, must be done in loop before: from 0 works
     url_t="https://www.google.at/search?q=site%3Awww.care.at+"+names['rename'][y]
     url.append(url_t)
     
@@ -34,4 +34,8 @@ number_df=pd.DataFrame(number)
 #print(names)
 frames=[names,number_df]
 all_df=pd.concat(frames,axis=1,join='outer')
+#writing all_df to xlsx
+writer=pd.ExcelWriter('D:/Dropbox/Dropbox/2_BTSync Sync/04_Uni/02_MA Arbeit/output.xlsx')
+all_df.to_excel(writer,'Sheet1')
+writer.save()
 print(all_df)
