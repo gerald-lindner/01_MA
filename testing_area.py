8905ec256100f748a94c19497aae1e4f7ca40a69
gerald-lindner <gerald.lindner@stud.sbg.ac.at>
2017-10-12 08:08:43 +0200

    before leaving to uni

--- testing_area.py
+++ testing_area.py
@@ -1,12 +1,24 @@
-import requests
-r = requests.get('http://icanhazip.com/')
-from stem import Signal
-from stem.control import Controller
+import scrapy
 
-with Controller.from_port(port = 9051) as controller:
-    controller.authenticate(password = 'geri')
-    controller.signal(Signal.NEWNYM)
-    controller.close()
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+    start_urls = (
+        'http://example.com/page1',
+        'http://example.com/page2',
+        )
 
-r = requests.get('http://icanhazip.com/')
-print(r.content)
+    def parse(self, response):
+        # collect `item_urls`
+        for item_url in item_urls:
+            yield scrapy.Request(item_url, self.parse_item)
+
+    def parse_item(self, response):
+        item = MyItem()
+        # populate `item` fields
+        # and extract item_details_url
+        yield scrapy.Request(item_details_url, self.parse_details, meta={'item': item})
+
+    def parse_details(self, response):
+        item = response.meta['item']
+        # populate more `item` fields
+        return item
